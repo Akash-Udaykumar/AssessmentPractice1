f3ea9c5f3415a7b46d19adfde1c4691c3487a360
Akash U k <akauk@publicisgroupe.net>
2022-11-29 20:36:37 +0530

    Practice 1 Assessment

---
+++ src/test/java/Pipeline/Pipe.java
@@ -0,0 +1,15 @@
+package Pipeline;
+
+import org.testng.annotations.Test;
+
+public class Pipe {
+	
+	
+	@Test
+	public void test() {
+		String fn= "Akash";
+		String ln= "Udaykumar";
+		System.out.println("Hello "+ fn+" "+ ln);
+	}
+
+}
